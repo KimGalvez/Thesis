fb282a73406ba832993af9bd6b1cd93260504dee
KimGalvez <galveezkim@gmail.com>
2024-09-19 21:38:57 +0800

    Add files via upload

---
+++ Layer/index.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!--=============== FAVICON ===============-->
+      <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+      <!--=============== REMIXICONS ===============-->
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css" rel="stylesheet">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+      <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+      <!--=============== CSS ===============-->
+      <link rel="stylesheet" href="css/style.css">
+
+      <title>WELCOME</title>
+   </head>
+
+   <body>
+      <!--==================== HEADER ====================-->
+        <header class="header" id="header">
+            <div class="toggle" id="toggle">
+                <i class='bx bx-menu'></i>
+            </div>
+
+            <div class="logo">
+                <a href=""><img src="img/logo.png" alt=""></a>
+            </div>
+        
+            <nav class="navbar" id="navbar">
+            
+                
+                <div class="close" id="close">
+                    <i class="ri-close-line"></i>
+                </div>
+            </nav>
+
+        
+        </header>
+
+
+        <!--==================== MAIN ====================-->
+        <main class="main">
+
+            <section class="main-layer">
+                <div class="select-layers">
+                    <label>
+                        <select>
+                            <option selected> LAYERS </option>
+                            <option value="layer 1">Layer 1</option>
+                            <option value="layer 2">Layer 2</option>
+                            <option value="layer 3">Layer 3</option>
+                            <option value="layer 4">Layer 4</option>
+                            <option value="layer 5">Layer 5</option>
+                            <option value="layer 6">Layer 6</option>
+                            <option value="layer 7">Layer 7</option>
+                            <option value="layer 8">Layer 8</option>
+                        </select>
+                        
+
+                        
+                    </label>
+                    <!--=============== BUTTON ===============-->
+                    <button class="btn"><span>Submit</span></button>
+                </div>
+
+                <input class="input-number" name="text" type="text" >
+
+                <div class="input">
+                    <div class="input-content">
+                        <h3>0 = OFF</h3>
+                        <h3>1 = ON</h3>
+
+                        <h3>2 = BLINK 5 TIMES</h3>
+                    </div>
+  
+                </div>
+            </section>
+
+            <section class="main-content">
+                <div class="content">
+                    <h3>LAYER 1</h3>    
+                    <h3>LAYER 2</h3> 
+                    <h3>LAYER 3</h3>      
+                    <h3>LAYER 4</h3> 
+                    <h3>LAYER 5</h3> 
+                    <h3>LAYER 6</h3> 
+                    <h3>LAYER 6</h3> 
+                    <h3>LAYER 7</h3> 
+                    <h3>LAYER 8</h3> 
+
+                    <button class="btn"><span>REFRESH</span></button>
+                </div>
+                
+            </section>
+        </main>
+
+    
+
+
+        <!--=============== MAIN JS ===============-->
+        <script src=""></script>
+   </body>
+</html>
+
+
